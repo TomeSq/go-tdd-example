ed1ff2a50ece39b8667a53cceb249c0a43c4ea42
kenichi_shintome <tome119@gmail.com>
2023-01-07 19:49:49 +0900

    addのテストを追加

---
+++ integers/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// 引数をSUMして返す
+func Add(x, y int) int {
+	return x + y
+}
---
+++ integers/adder_test.go
@@ -0,0 +1,14 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	t.Parallel()
+
+	sum := Add(2, 2)
+	excepted := 4
+
+	if sum != excepted {
+		t.Errorf("excepted '%d' but got '%d'", excepted, sum)
+	}
+}
