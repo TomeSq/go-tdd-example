cc20187fb14e0bc46f1fa48fb7d99b8d35083666
kenichi_shintome <tome119+github@gmail.com>
2023-03-14 22:41:29 +0000

    既存のキーで追加された場合エラーを返すようにした。

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,6 +19,27 @@ func Test_検索(t *testing.T) {
 	})
 }
 
+func Test_追加(t *testing.T) {
+	dictionary := Dictionary{}
+	word := "test"
+	definition := "this is just a test"
+
+	t.Run("新規追加", func(t *testing.T) {
+		dictionary.Add(word, definition)
+
+		assertDefinition(t, dictionary, word, definition)
+	})
+
+	t.Run(`既存追加`, func(t *testing.T) {
+		dictionary.Add(word, definition)
+
+		err := dictionary.Add(word, "new test")
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
@@ -32,5 +53,25 @@ func assertError(t *testing.T, got, want error) {
 
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
+	}
+
+	if got == nil {
+		if want != nil {
+			return
+		}
+		t.Fatal("expected to get an error.")
+	}
+}
+
+func assertDefinition(t *testing.T, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
